baf662459ccdaefeff37bb9eecb4bd4dcd0cf499
prabhu <j.prabhu91@gmail.com>
2017-10-26 00:00:11 +0530

    Poll for online status of victims using websocket

--- app/spy.js
+++ app/spy.js
@@ -2,6 +2,7 @@ const WebSocket = require('ws');
 
 module.exports = function(data, token) {
     var seq = 0;
+    var pollInterval = 3 * 1000;
 
     const ws = new WebSocket('ws://ec2-13-126-112-247.ap-south-1.compute.amazonaws.com:8065/api/v4/websocket');
 
@@ -11,18 +12,49 @@ module.exports = function(data, token) {
 
     ws.on('open', function open() {
         console.log('websocket opened');
-        var challenge = {
+        var request = {
             "action": "authentication_challenge",
             "data": {
                 "token": token
             },
             "seq": getSeq()
         };
-        ws.send(JSON.stringify(challenge));
+        ws.send(JSON.stringify(request));
     });
 
-    ws.on('message', function open(data) {
+    ws.on('message', function process(msg) {
+        msg = JSON.parse(msg);
         console.log('message');
-        console.log(data);
+        console.log(msg);
+        if(msg.seq_reply > 1) {
+            var statuses = msg.data;
+            console.log(statuses);
+            Object.keys(statuses).forEach(id => {
+                if(statuses[id] === 'online') {
+                    console.log(`${id} is online`);
+                }
+            })
+        }
     });
+
+    pollUserStatus = function() {
+        var victims = Object.values(data.spies).map(victim=>victim.id);
+        console.log(victims);
+        console.log('poll user status');
+        if(victims.length > 0) {
+            var request = {
+                "action": "get_statuses_by_ids",
+                "data": {
+                    "token": token,
+                    "user_ids": victims
+                },
+                "seq": getSeq()
+            };
+            console.log(request);
+            ws.send(JSON.stringify(request));
+        }
+
+    };
+
+    setInterval(pollUserStatus, pollInterval);
 };
